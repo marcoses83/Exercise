287b2c347c77aaad54b29b083e2bfaa4c37bdaeb
Marcos Escribano <marcoses83@gmail.com>
2018-01-14 03:17:35 +0100

    Get city from Geonames

--- src/main/java/pa/sher/service/GeonamesService.java
+++ src/main/java/pa/sher/service/GeonamesService.java
@@ -1,45 +1,50 @@
 package pa.sher.service;
 
-import javax.jws.WebService;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class GeonamesService {
     private static String username = "marcoses83";
-    private static int maxResults = 5;
+    private static String countryIdo2 = "ES";
 
-    public static String getCity(String postalcode) {
+    public static String getCity(String postalcode, String countryIso2) {
         String city = "";
-        WebService.setUserName(username);
-        try {
-            PostalCodeSearchCriteria searchCriteria = new PostalCodeSearchCriteria();
-            searchCriteria.setCountryCode("ES");
-            searchCriteria.setPostalCode(postalcode);
-            searchCriteria.setMaxRows(maxResults);
-            List<PostalCode> pcodes = WebService.postalCodeSearch(searchCriteria);
-            city = pcodes.get(0).getPlaceName();
-        } catch (Exception e) {
 
+        try {
+            StringBuilder url = new StringBuilder();
+            url.append("http://api.geonames.org/postalCodeSearch?");
+            url.append("postalcode=").append(postalcode);
+            url.append("&country=").append(countryIso2);
+            url.append("username=").append(username);
+
+            String xmlResponse = getApiRequest(url.toString());
+            DocumentBuilderFactory docBFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBFactory.newDocumentBuilder();
+            InputStream stream = new ByteArrayInputStream(xmlResponse.getBytes("UTF-8"));
+            Document doc = docBuilder.parse(stream);
+            city = doc.getElementsByTagName("name").item(0).getTextContent();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         return city;
     }
 
-    public static String getApiRequest(String url) throws IOException {
-
+    private static String getApiRequest(String url) throws IOException {
         HttpURLConnection urlConnection = (HttpURLConnection) new URL(url).openConnection();
         try {
             urlConnection.setRequestMethod("GET");
             urlConnection.connect();
 
             InputStream stream;
-            if (HttpStatus.SC_OK <= urlConnection.getResponseCode() && urlConnection.getResponseCode() < HttpStatus.SC_MULTIPLE_CHOICES) {
+            if (200 <= urlConnection.getResponseCode() && urlConnection.getResponseCode() < 300) {
                 stream = urlConnection.getInputStream();
-            } else if (urlConnection.getResponseCode() == HttpStatus.SC_NOT_FOUND) {
+            } else if (urlConnection.getResponseCode() == 404) {
                 return null;
             } else {
                 stream = urlConnection.getErrorStream();
@@ -67,7 +72,7 @@ public class GeonamesService {
         }
     }
 
-    public static String getCity(String postalcode) {
+    /*public static String getCity(String postalcode) {
         return "Bilbao";
-    }
+    }*/
 }
