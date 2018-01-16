ded1f97ef7fde8e249f98db0cea83810e1a4e246
Marcos Escribano <marcos.escribano@ve.com>
2018-01-16 18:21:28 +0100

    Added stub of DataRepositoryTest

---
+++ src/test/java/data/DataRepositoryTest.java
@@ -0,0 +1,19 @@
+package data;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.FileWriter;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DataRepositoryTest {
+    @Mock
+    FileWriter fileWriter;
+
+    @Test
+    public void testSaveUserLocation() {
+
+    }
+}
